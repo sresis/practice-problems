4da346cb9756872c5b79ccfd8a538ba29ce9e073
Stephanie Resis <sresis@umich.edu>
2020-10-09 14:45:25 -0700

    completed problem

--- sort-ab/sortab.py
+++ sort-ab/sortab.py
@@ -25,6 +25,22 @@ def sort_ab(a, b):
 
     You may not use sorted() or .sort().
     """
+    # start in list b and go through it
+    # ffor each item in list b, see if it is greater than a each number in a. 
+    # if it is not, add it
+    # you can then start at the next index for the next number in b
+    tracker = 0
+    for num in b:
+        i = tracker
+        if len(a) == 0 or num > max(a):
+            a.append(num)
+        else:
+            while num > a[i]:
+                i += 1
+            a.insert(i, num)
+            tracker = i
+    return a
+
 
 
 if __name__ == '__main__':
