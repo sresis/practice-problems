859e022e8a1fc2f651fa6640ed55f8ee5d3dcb80
Stephanie Resis <sresis@umich.edu>
2020-10-12 11:46:34 -0700

    add file

--- balanced-brackets/balancedbrackets.py
+++ balanced-brackets/balancedbrackets.py
@@ -48,11 +48,38 @@ If you receive a string with no brackets, consider it balanced::
 
 
 def has_balanced_brackets(phrase):
-    """Does a given string have balanced pairs of brackets?
+   """Does a given string have balanced pairs of brackets?
 
-    Given a string as input, return True or False depending on whether the
-    string contains balanced (), {}, [], and/or <>.
-    """
+   Given a string as input, return True or False depending on whether the
+   string contains balanced (), {}, [], and/or <>.
+   """
+
+   # the closing bracket should not come before the 
+   # bracket_dict = {'>': [], '<': [], '(': [], ')': [], '<': [], '>': [],
+   # '{': [], '}': [] }
+   b_dict = {}
+   brackets = ['[', ']', '<', '>', '(', ')', '{', '}']
+   pairs = {'(': ')', '{': '}', '[': ']', '<': '>'}
+   # get bracket pairs
+   # then see if its match is in i:n
+   n = len(phrase)
+   for i in range(0,len(phrase)):
+      if phrase[i] in pairs:
+         if pairs[phrase[i]] not in phrase[i:n]:
+            print('s')
+            return False
+
+         # otherwise, update n with the location of the pair
+   return True
+
+   # alternative way
+   # get the first bracket
+   # make sure the closer for that one is the last bracket
+   # the second bracket closer shouldn be second to last bracket, etc
+
+   # store recent bracket outward
+   # if the closing is not the most recent outward
+   
 
 if __name__ == '__main__':
     import doctest
