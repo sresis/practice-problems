a2c0e61f8c5d992bad88a9832a548823168d5bc4
Stephanie Resis <sresis@umich.edu>
2020-10-19 14:04:24 -0700

    completed problem

---
+++ URLIFY/URLify.py
@@ -0,0 +1,27 @@
+"""Write a method to replace all spaces in a string with '%20'.
+You may assume that you are given the "true" length of the string
+
+   >>> urlify('Mr John Smith', 13)
+   'Mr%20John%20Smith'
+   >>> urlify('Hello 20', 8)
+   'Hello%2020'
+
+
+"""
+def urlify(string, num):
+    #turn it into array
+    # if it is not the last one, add %20
+    word_arr = string.split(' ')
+    new_str = ''
+    for i in range(len(word_arr)-1):
+        new_str += word_arr[i]
+        new_str += '%20'
+    new_str += word_arr[-1]
+    return new_str
+
+   
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED!\n")
