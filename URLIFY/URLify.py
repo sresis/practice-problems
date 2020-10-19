ab0069c82e41724d882812d92ea3f481a4af4d02
Stephanie Resis <sresis@umich.edu>
2020-10-19 14:11:42 -0700

    completed problem

--- URLIFY/URLify.py
+++ URLIFY/URLify.py
@@ -8,17 +8,28 @@ You may assume that you are given the "true" length of the string
 
 
 """
+# alternate approach
+# def urlify(string, num):
+#     #turn it into array
+#     # if it is not the last one, add %20
+#     word_arr = string.split(' ')
+#     new_str = ''
+#     for i in range(len(word_arr)-1):
+#         new_str += word_arr[i]
+#         new_str += '%20'
+#     new_str += word_arr[-1]
+#     return new_str
 def urlify(string, num):
-    #turn it into array
-    # if it is not the last one, add %20
-    word_arr = string.split(' ')
-    new_str = ''
-    for i in range(len(word_arr)-1):
-        new_str += word_arr[i]
-        new_str += '%20'
-    new_str += word_arr[-1]
-    return new_str
-
+    # count spaces
+    space_count = 0
+    for item in string:
+        if item == ' ':
+            space_count += 1
+    new_len = num - space_count + (3 * space_count)
+    print(new_len)
+    # go through string backwards
+    char_count = num
+    # if character is a space, increase char count by 2 and add the '%20'
    
 
 if __name__ == '__main__':
