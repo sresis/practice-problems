639ddc178d83a87a802c813623e58cc69ca6cf0b
Stephanie Resis <sresis@umich.edu>
2020-10-26 11:00:26 -0700

    alternate solution

--- balanced-parentheses/balancedparentheses.py
+++ balanced-parentheses/balancedparentheses.py
@@ -29,27 +29,17 @@ If you receive a string with no parentheses, consider it balanced::
 
 def has_balanced_parens(phrase):
     """Does a string have balanced parentheses?"""
-    # count outer and count innter
-    # if the counts equal, return true
-    # if dict is blank, return true
-    par_dict = {}
+    # keep track of how many are open
+    parens = 0
     for item in phrase:
-        if item == '(' or item == ')':
-            if item in par_dict:
-                par_dict[item] += 1
-            else:
-                par_dict[item] = 1
-    if par_dict:
-        if '(' not in par_dict or ')' not in par_dict:
-            return False
-        if par_dict['('] == par_dict[')']:
-            return True
-        else:
-            return False
-
+        if item == '(':
+            parens += 1
+        elif item == ')':
+            parens -= 1
+    if parens != 0:
+        return False
     else:
         return True
-
 
 if __name__ == '__main__':
     import doctest
