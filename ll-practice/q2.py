496821cc5ebde4f56574a335bac3f6a9ee3c0098
Stephanie Resis <sresis@umich.edu>
2020-11-03 10:32:05 -0800

    complete q2

---
+++ ll-practice/q2.py
@@ -0,0 +1,20 @@
+"""
+Define two classes:
+
+DblNode — a node in a doubly-linked list
+
+DblLinkedList — a doubly-linked list with a head and tail
+
+You are only required to define the __init__ method for each class.
+"""
+class DblNode(object):
+    def __init__(self, data, next, prev:
+    self.data = data
+    self.next = None
+    self.prev = None
+
+class DblLinkedList(object):
+
+    def __init__(self):
+         self.head = None
+         self.tail = None
